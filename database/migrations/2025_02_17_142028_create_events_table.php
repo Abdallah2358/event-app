99b87263ee0a6219b48a62134c2349543f6ea2dd
عبدالله الحديدي <abdallah201@protonmail.com>
2025-02-17 21:17:36 +0200

    added events schema

--- database/migrations/2025_02_17_142028_create_events_table.php
+++ database/migrations/2025_02_17_142028_create_events_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->dateTime('scheduled_at');
+            $table->unsignedSmallInteger('days');
+            $table->time('start');
+            $table->time('end');
+            $table->unsignedInteger('capacity')->default(100);
+            $table->unsignedInteger('waitlist_capacity')->default(1000);
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->enum('status', ['live', 'draft']);
             $table->timestamps();
         });
     }
